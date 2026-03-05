b1227b51b12ddc33b2c535f931f78ae61f6303e7
sultandevin <sultan.suyudi@gmail.com>
2026-03-05 14:06:51 +0700

    fix(middleware): allow admins to access premium routes

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -8,41 +8,45 @@ import { requireAdmin, requireSuperAdmin } from "./middlewares/rbac";
 
 export const pub = o;
 const requireAuth = o.middleware(async ({ context, next, errors }) => {
-	if (!context.session?.user) throw errors.UNAUTHORIZED();
+  if (!context.session?.user) throw errors.UNAUTHORIZED();
 
-	// Reset premium status if expired
-	if (
-		context.session.user.isPremium &&
-		context.session.user.premiumExpiresAt &&
-		context.session.user.premiumExpiresAt.getTime() < Date.now()
-	) {
-		await db
-			.update(user)
-			.set({ isPremium: false })
-			.where(eq(user.id, context.session.user.id))
-			.then(() => {
-				context.session!.user.isPremium = false;
-			});
-	}
+  // Reset premium status if expired
+  if (
+    context.session.user.isPremium &&
+    context.session.user.premiumExpiresAt &&
+    context.session.user.premiumExpiresAt.getTime() < Date.now()
+  ) {
+    await db
+      .update(user)
+      .set({ isPremium: false })
+      .where(eq(user.id, context.session.user.id))
+      .then(() => {
+        context.session!.user.isPremium = false;
+      });
+  }
 
-	return next({
-		context: {
-			session: context.session,
-		},
-	});
+  return next({
+    context: {
+      session: context.session,
+    },
+  });
 });
 
 const requirePremium = o.middleware(({ context, next, errors }) => {
-	if (!context.session?.user.isPremium)
-		throw errors.FORBIDDEN({
-			message: "Akun premium dibutuhkan untuk mengakses resource ini.",
-		});
+  if (
+    !context.session?.user.isPremium &&
+    context.session?.user.role !== "admin" &&
+    context.session?.user.role !== "superadmin"
+  )
+    throw errors.FORBIDDEN({
+      message: "Akun premium dibutuhkan untuk mengakses resource ini.",
+    });
 
-	return next({
-		context: {
-			session: context.session,
-		},
-	});
+  return next({
+    context: {
+      session: context.session,
+    },
+  });
 });
 
 export const authed = pub.use(requireAuth);
@@ -51,8 +55,8 @@ export const admin = authed.use(requireAdmin);
 export const superadmin = authed.use(requireSuperAdmin);
 
 const rateLimit = createRatelimitMiddleware({
-	limiter: ({ context }) => (context.session.user.isPremium ? premiumRatelimiter : freeRatelimiter),
-	key: ({ context }) => context.session.user.id,
+  limiter: ({ context }) => (context.session.user.isPremium ? premiumRatelimiter : freeRatelimiter),
+  key: ({ context }) => context.session.user.id,
 });
 
 export const authedRateLimited = authed.use(rateLimit);
