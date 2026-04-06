bd3910e9c439493f94cd04038dcda478b60220fa
Andreandhiki <andreandhikiriyantap@mail.ugm.ac.id>
2026-04-06 22:30:39 +0700

    fix(testimoni): ganti ui dan data baru

--- apps/web/src/routes/-home/testimone.tsx
+++ apps/web/src/routes/-home/testimone.tsx
@@ -1,10 +1,12 @@
-import Carousel from "@/components/shared/carousel";
+// import Carousel from "@/components/shared/carousel";
+import { Container } from "@/components/ui/container";
 import { Heading } from "./-components/heading";
 import { DATA } from "./data";
+import { Image } from "@unpic/react";
 
 export default function Testimone() {
 	return (
-		<section className="overflow-x-clip">
+		<Container className="max-w-7xl overflow-x-clip">
 			<div className="mb-4 text-center">
 				<Heading>
 					Hasil Nyata dari Strategi Belajar <span className="font-semibold text-secondary-700">Bimbel Beta</span>
@@ -22,7 +24,7 @@ export default function Testimone() {
 				))}
 			</div>
 
-			<Carousel
+			{/*<Carousel
 				items={[...DATA.testimone]}
 				showNavigation={true}
 				showDots={true}
@@ -30,7 +32,39 @@ export default function Testimone() {
 				gap={35}
 				responsiveGap={true}
 				className=""
-			/>
-		</section>
+			/>*/}
+			<TestimoneCard data={DATA.testimone} />
+		</Container>
+	);
+}
+
+function TestimoneCard({ data }: { data: typeof DATA.testimone }) {
+	const placeholder = "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=400";
+
+	return (
+		<div className="grid gap-6 sm:grid-cols-2">
+			{data.map((t) => (
+				<div key={t.id} className="flex flex-col md:flex-row items-start gap-4 p-4 bg-white border border-0.5 rounded-lg md:h-60">
+					<Image
+						src={t.avatar ?? placeholder}
+            alt={`${t.name} avatar`}
+						width={1000}
+            height={1000}
+						className="md:w-40 h-60 md:h-full rounded-lg object-top object-cover aspect-3/4"
+					/>
+
+					<div className="h-full flex flex-col justify-between">
+						<div className="mb-3 pr-2 overflow-y-scroll max-h-30 lg:max-h-40">
+							<p className="text-gray-500 text-sm">{t.desc}</p>
+						</div>
+
+						<div className="mb-0 mt-auto">
+							<p className="text-base md:text-lg font-medium">{t.name}</p>
+							<p className="text-xs md:text-sm text-gray-400">{t.title}</p>
+						</div>
+					</div>
+				</div>
+			))}
+		</div>
 	);
 }
